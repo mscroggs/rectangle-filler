9e7650124c347f03b689294bbb6ce1132e15e382
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2026-04-27 20:38:00 +0100

    improve code - but still v. slow

--- rect.py
+++ rect.py
@@ -20,9 +20,9 @@ def fill(
     placed: list[tuple[int, int, bool]] = [],
     remaining: list[tuple[int, int]] | None = None,
     bound: int | None = None,
+    info: str = "",
 ) -> list[list[tuple[int, int, bool]]]:
-    if bound is not None and len(placed) > bound - 5:
-        print(placed)
+    print(info)
     if remaining is None:
         remaining = [(i, j) for i in range(large[0] + 1) for j in range(large[1] + 1)]
     if len(placed) == 0:
@@ -31,24 +31,27 @@ def fill(
         start = placed[-1]
     results = [placed]
 
-    for (i, j) in remaining:
+    for index, (i, j) in enumerate(remaining):
         for o in [False, True]:
-            if i + (small[0] if o else small[1]) > large[0]:
+            d = small if o else small[::-1]
+            if i + d[0] > large[0] or j + d[1] > large[1]:
                 continue
-            if j + (small[1] if o else small[0]) > large[1]:
-                continue
-            if all(not overlaps(small, (i, j, o), p) for p in placed):
-                x = (i, i + (small[0] if o else small[1]))
-                y = (j, j + (small[1] if o else small[0]))
+            if True: # all(not overlaps(small, (i, j, o), p) for p in placed):
+                x = (i, i + d[0])
+                y = (j, j + d[1])
+                if all((a, j - 1) in remaining for a in range(*x)) or all((i - 1, a) in remaining for a in range(*y)):
+                    continue
                 rem = [(a, b) for a, b in remaining if not (x[0] <= a < x[1] and y[0] <= b < y[1]) and (a > i or (a == i and b > j))]
                 if len(placed) + len(rem) // (small[0] * small[1]) < (len(results[0]) if bound is None else bound):
                     return results
-                for res in fill(large, small, placed + [(i, j, o)], rem, bound=bound):
-                    if len(res) > len(results[0]):
+                for res in fill(large, small, placed + [(i, j, o)], rem, bound=bound, info=f"{info} {index}/{len(remaining)}"):
+                    if len(res) > len(results[0]) and len(res) >= bound:
+                        print(len(res), res)
                         results = [res]
                     elif len(res) == len(results[0]):
+                        print(len(res), res)
                         results.append(res)
                     if bound is not None and len(res) > bound:
                         with open("found.txt", "a") as f:
-                            f.write(json.dumps(res) + "\n")
+                            f.write(f"{len(res)} -> " + json.dumps(res) + "\n")
     return results
--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+import pytest
 from rect import fill
 
 
@@ -8,3 +9,20 @@ def test_2_by_1():
     assert len(results) == 2
     assert len(results[0]) == 2
     assert len(results[1]) == 2
+
+@pytest.mark.parametrize("n", [1, 2, 4, 8, 16, 32, 64])
+@pytest.mark.parametrize("m", [1, 2, 4, 8, 16, 32, 64])
+def test_halves(n, m):
+    results = fill((2*n, m), (m, n))
+    for r in results:
+        print(r)
+        assert len(r) == 2
+
+
+@pytest.mark.parametrize("n", [4, 8, 16, 32, 64])
+@pytest.mark.parametrize("m", [4, 8, 16, 32, 64])
+def test_halves_add_1(n, m):
+    results = fill((2*n + 1, m), (m, n))
+    for r in results:
+        print(r)
+        assert len(r) == 2
